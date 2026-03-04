8d4085a9f77c1fafa46a02cdbcda2f916a1d884f
Jthummar2103 <jeelthummar222@gmail.com>
2026-03-04 11:50:00 -0500

    configure express app and server entry point

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+
+require("dotenv").config();
+require("./config/db");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Student Resource API Running" });
+});
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/categories", require("./routes/categories"));
+app.use("/api/resources", require("./routes/resources"));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/stats", require("./routes/stats"));
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Server error" });
+});
+
+module.exports = app;
